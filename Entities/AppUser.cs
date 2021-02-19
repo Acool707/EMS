8487bc104f72384f941c22f51919777db2bc5975
Acool707 <59957657+Acool707@users.noreply.github.com>
2021-02-19 12:54:42 +0530

    company added

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -17,6 +17,7 @@ namespace EMS.Entities
         public string UserType { get; set; }
         public string Department { get; set; }
         public string Location { get; set; }
+        public string Company { get; set; }
         public string Email { get; set; }
         public string  PhoneNumber { get; set; }
         public DateTime CreatedAt { get; set; }
